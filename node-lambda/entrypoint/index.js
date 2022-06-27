11e67153bce6aece381f93f548ff8ee7109dbada
Brad <bstaszcuk@backblaze.com>
2022-06-27 12:52:55 -0500

    creating new Lambda option. inital commit

---
+++ node-lambda/entrypoint/index.js
@@ -0,0 +1,116 @@
+const AWS = require('aws-sdk');
+const fetch = require('node-fetch');
+
+async function fetchAssetInfo (id) {
+
+    const token = process.env.FRAMEIO_TOKEN;
+
+    let path = `https://api.frame.io/v2/assets/${id}`;
+    let requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    };
+    try {
+        let response = await fetch(path, requestOptions);
+        let result = await response.json();
+        console.log(`More than one object?: ${result.length}`);
+        //console.log('result: ' + JSON.stringify(result, null, 2));
+        //console.log(`length: ${Object.keys(result).length}`);
+
+        if (result._type == 'version_stack') {
+            console.log(`version_stack detected: $(result.id)`);
+            let {url, name} = await fetchAssetInfo(result.id + '/children');
+            //console.log('in stack ' + JSON.stringify(versionStack));
+            console.log("version_stack processing finished");
+            return { url: '', name: "Version Stack named: " + result.name };
+        }
+
+        if ( result.length ) { // more than one result means it's a folder or version_stack and we need to iterate
+            for (const item of Object.keys(result)) {
+                    console.log(`version_stack child item type: ${result[item]._type}`);
+                    if (result[item]._type == 'file') {
+                        console.log(`version_stack child upload : ${result[item].name} `);
+                        await invokeLambda(result[item].original, result[item].name);
+                    } else {
+                        console.log("version_stack child type not supported, or not found");
+                        //console.log(`version_stack child full :` + JSON.stringify(item, null, 2));
+                        throw `archiveType: ${result[item]} version_stack child unknown or project_id : ${result[item].project_id} not found `;
+                    }
+            }
+            return{};
+
+        } else {
+            console.log(`begin typing: ${result._type}`);
+            if (result._type == 'file') {
+                console.log(`item is file type: ${result.name}`);
+                return { url: result.original, name: result.name };
+            } else {
+                console.log("type not supported, or not found");
+                //console.log(`printout full :` + JSON.stringify(result, null, 2));
+                throw `archiveType: ${result._type} unknown or project_id : ${result.project_id} not found `;
+            }
+        }
+    } catch(err) {
+        console.log(`error received: ${err}`);
+        if ( err.startsWith("archiveType: ") ) {
+            return { url: err };
+        }
+        return (`error: ${err}`);
+    }
+}
+
+function invokeLambda (url, name) { 
+
+    const lambda = new AWS.Lambda();
+
+    let req = {
+        FunctionName: 'upload-to-b2',
+        InvocationType: 'Event', // returns statusCode 202 on success. See invoke() SDK for info
+        Payload: JSON.stringify({
+            url: url, 
+            name: name })
+    };
+
+    return lambda.invoke(req).promise();
+}
+
+exports.handler = async function (event, context) {
+    //const caller = context.functionName;
+    console.log(JSON.stringify(event));
+    
+    let id = JSON.parse(event.body).resource.id;
+    let { url, name} = await fetchAssetInfo(id);
+
+    try {
+        if ( typeof url !== 'undefined' && url ) {
+            await invokeLambda(url, name);
+        }
+
+        if ( url.startsWith("archiveType: ")) {
+            let returnPayload = {
+                statusCode: 202, 
+                body: JSON.stringify({
+                    'title': `Job rejected.`,
+                    'description': `Only "file" archive is currently implemented, not ${url}. Coming soon!`
+                })
+            };
+            return returnPayload;
+        }
+
+        let returnPayload = {
+            statusCode: 202, 
+            body: JSON.stringify({
+                'title': `Job received!`,
+                'description': `Your archive job for '${name}' has been triggered.`
+            })
+        };
+        return returnPayload;
+
+    } catch(err) {
+        console.log(`ERROR Hit a problem: ${err.message}`);
+        return err;
+    }
+};
