5d291b0c0076233f7e272675cdd77632f1895fd1
Brad <bstaszcuk@backblaze.com>
2022-07-08 13:29:31 -0500

    adding in sig checking and cleanup

---
+++ node-docker/README.md
@@ -0,0 +1,19 @@
+# Backblaze / frame.io connector
+
+This is a Docker version of the 'Archive to Backblaze' connector for frame.io
+
+We specifically tested the container using [Rising Cloud](https://risingcloud.com/) as a web service, but it should run on any container platform. 
+
+If using Rising Cloud, please override the default autoscaling and set a minimum of '1' worker to be ready to serve requests. You will also want to set the 'Expose Port' to 8675. We leverage the container platform to handle TLS termination. If you want to expose the container directly we **strongly** recommend you implement TLS within the app code directly. 
+
+***
+
+You need to set 5 environment variables in the container environment:
+
+- FRAMEIO_TOKEN = The frame.io developer /token/ you obtain from https://developer.frame.io/app/tokens
+- FRAMEIO_SECRET = the frameio Custom Action /secret/ you obtain from https://developer.frame.io/app/custom-actions
+- BUCKET_ENDPOINT = Your Backblaze B2 S3-compatible endpoint, in the form s3.REGION.backblazeb2.com
+- BUCKET_NAME = Your Backblaze B2 bucket name
+- ACCESS_KEY = Your Backblaze B2 access key - it is strongly recommended this is unique for this app
+- SECRET_KEY = Your Backblaze B2 secret key - it is strongly recommended this is unique for this app
+
--- node-docker/index.js
+++ node-docker/index.js
@@ -6,13 +6,12 @@ var crypto = require('crypto');
 
 const envVars = ['FRAMEIO_TOKEN', 'FRAMEIO_SECRET', 'BUCKET_ENDPOINT', 'BUCKET_NAME', 'ACCESS_KEY', 'SECRET_KEY'];
 
-const app = express();
 
 async function fetchAssetInfo (id) {
-
+    // get asset info based on the id from frame.io
     const token = process.env.FRAMEIO_TOKEN;
 
-    console.log("asset id: " + id)
+    console.log('asset id: ', id)
 
     let path = `https://api.frame.io/v2/assets/${id}`;
     let requestOptions = {
@@ -23,48 +22,49 @@ async function fetchAssetInfo (id) {
       }
     };
 
-    console.log("asset path: " + path)
+    console.log('asset path: ', path)
 
     try {
         let response = await fetch(path, requestOptions);
         let result = await response.json();
 
-        console.log(`if defined, more than one item: ${result.length}`);
+        console.log('number of items: ', result.length);
 
         if (result._type == 'version_stack') {
-            console.log(`version_stack detected, processing stack: ${result.id}`);
+            console.log('version_stack id: ', result.id);
+            // recursively call to iterate over children
             return {url, name} = await fetchAssetInfo(result.id + '/children');
         }
 
         if (result.length) { // more than one result means it's a folder or version_stack and we need to iterate
             for (const item of Object.keys(result)) {
-                    console.log(`version_stack child item type: ${result[item]._type}`);
+                    console.log('version_stack child type: ', result[item]._type);
                     if (result[item]._type == 'file') {
-                        console.log(`version_stack child upload : ${result[item].name} `);
-                        invokeUploader(result[item].original, result[item].name);
+                        console.log('version_stack child uploading: ', result[item].name);
+                        streamUpload(result[item].original, result[item].name);
                     } else {
-                        console.log("version_stack child type not supported, or not found");
-                        //console.log(`version_stack child full :` + JSON.stringify(item, null, 2));
+                        console.log('version_stack child type unknown');
+                        //console.log(`version_stack child contains :` + JSON.stringify(item, null, 2));
                         throw `archiveType: ${result[item]} version_stack child unknown or project_id : ${result[item].project_id} not found `;
                     }
             }
-            console.log("version_stack processing finished");
-            return { url: '', name: "Version Stack" }; // using empty url value for logic in main
+            console.log('version_stack completed');
+            return { url: '', name: 'Version Stack' }; // using empty url value for logic in main
 
 
         } else {
-            console.log(`begin typing: ${result._type}`);
+            console.log('begin typing: ', result._type);
             if (result._type == 'file') {
-                console.log(`item is file type: ${result.name}`);
+                console.log('item is file type: ', result.name);
                 return { url: result.original, name: result.name };
             } else {
-                console.log("type not supported, or not found");
+                console.log('type not supported, or not found');
                 //console.log(`printout full :` + JSON.stringify(result, null, 2));
                 throw `archiveType: ${result._type} unknown or project_id : ${result.project_id} not found `;
             }
         }
     } catch(err) {
-        console.log(`error received: ${err}`);
+        console.log('error received: ', err);
         if ( err.startsWith("archiveType: ") ) {
             return { url: err };
         }
@@ -72,12 +72,12 @@ async function fetchAssetInfo (id) {
     }
 };
 
-async function invokeUploader (url, name) { 
+async function streamUpload (url, name) { 
 
-    console.log(`upload triggered: ${name}...`);
+    console.log('upload triggered: ', name);
 
     try {
-        const { writeStream, promise } = b2Uploader({ name, url });
+        const { writeStream, promise } = createWriteStream(url, name);
 
         fetch(url)
             .then(response => {
@@ -95,10 +95,10 @@ async function invokeUploader (url, name) {
         throw (`upload failed error: ${err}`);
     }
     
-    return (console.log(`Done uploading ${name}!`));
+    return (console.log('Done uploading: ', name));
 };
 
-const b2Uploader = ({ name, url }) => {
+const createWriteStream = (url, name) => {
 
     var endpoint = new AWS.Endpoint('https://' + process.env.BUCKET_ENDPOINT);
 
@@ -127,77 +127,99 @@ const b2Uploader = ({ name, url }) => {
             }).promise()
         };
     } catch(err) {
-        console.log(`b2uploader failed : ${err}`)
-        throw (`b2uploader failed : ${err}`);
+        console.log('createWriteStream failed : ', err)
+        throw ('createWriteStream failed : ', err);
     }
 };
 
-function matchingHMAC (stringToHash) {
-    var hmac = crypto.createHmac('sha256', process.env.FRAMEIO_SECRET);
-    console.log(stringToHash);
-    data = hmac.update(stringToHash);
-    gen_hmac = data.digest('hex');
-    console.log("hmac:" + gen_hmac);
-    return(gen_hmac);
+function calcHMAC (stringToHash) {
+    //calculate SHA256 HMAC of string.
+    try {
+        var hmac = crypto.createHmac('sha256', process.env.FRAMEIO_SECRET);
+        console.log(stringToHash);
+        data = hmac.update(stringToHash);
+        gen_hmac = data.digest('hex');
+        console.log('hmac: ', gen_hmac);
+        return(gen_hmac);
+    } catch(err) {
+        console.log('ERROR calcHMAC: ', err.message);
+        throw err;
+    }
 };
 
+function checkEnvVars (req, res, next) {
+    // make sure the environment variables are set
+    try {
+        envVars.forEach(element => {
+            console.log('checking: ', element);
+            if (!process.env[element] || process.env[element] == '') {
+                throw('Environment variable: ', element);
+            };
+        });
+        next();
+    } catch(err) {
+        console.log('ERROR checkEnvVars: ', err);
+        throw res.status(500).json({'error': 'internal configuration'});
+    };
+};
+
+function frameSignatureCheck (req, res, next) {
+    // check signature for frameio
+    try {
+        // frame.io signature format is 'v0:timestamp:body'
+        let sigString = 'v0:' + req.header("X-Frameio-Request-Timestamp") + ':' + JSON.stringify(req.body);
+
+        //console.log(calcHMAC(sigString));
+        //console.log("v0=" + req.header("X-Frameio-Signature"));
+
+        //check to make sure the signature matches, or finish
+        if (("v0=" + calcHMAC(sigString)) != (req.header("X-Frameio-Signature"))) {
+            return res.status(403).json({'error': 'mismatched hmac'});
+        };
+        next();
+    } catch(err) {
+        console.log('ERROR frameSignatureCheck: ', err.message);
+        throw new Error(err);
+    };
+};
+
+const app = express();
 app.use(express.json()); 
 
-app.post('/', async (req, res) => {
+app.post('/', [frameSignatureCheck, checkEnvVars], async (req, res) => {
 
     //console.log('body: ' + JSON.stringify(req.body));
     //console.log('headers: ' + JSON.stringify(req.headers));
 
-    let sigString = 'v0:' + req.header("X-Frameio-Request-Timestamp") + ':' + JSON.stringify(req.body);
-
-    //console.log(matchingHMAC(sigString));
-    //console.log("v0=" + req.header("X-Frameio-Signature"));
-    if (("v0=" + matchingHMAC(sigString)) != (req.header("X-Frameio-Signature"))) {
-        res.status(403);
-        res.json({'error': 'mismatched hmac'});
-        return;
-    };
-
-    // make sure the environment variables are set.
-    envVars.forEach(element => {
-        console.log(`checking ${element} is set`);
-        if (!process.env[element]) {
-            throw(`Environment variable ${element} not set`);
-        };
-    });
-
     let id = req.body.resource.id;
     let { url, name} = await fetchAssetInfo(id);
 
-    console.log(`url is ${url}`);
-    console.log(`name is ${name}`);
+    console.log('url: ', url);
+    console.log('name: ', name);
+
     try {
         if ( typeof url !== 'undefined' && url ) {
-            invokeUploader(url, name);
+            streamUpload(url, name);
         }
 
+        // send a 200 on rejection so frameio will display the msg
         if ( url.startsWith("archiveType:")) {
-            res.status(202);
-            res.json({
-                    'title': 'Job rejected.',
-                    'description': `${url} not supported.`
+            res.status(200).json({
+                'title': 'Job rejected.',
+                'description': `${url} not supported.`
             });
         } else {
-
-            res.status(202);
-            res.json({
+            res.status(202).json({
                 'title': 'Job received!',
                 'description': `Archive job for '${name}' has been triggered.`
             });
         };
 
     } catch(err) {
-        console.log(`ERROR Hit a problem: ${err.message}`);
-        res.status(500);
-        res.json(`ERROR: ${err.message}`);
+        console.log('ERROR / POST: ', err.message);
+        res.status(500).json({'error': 'error processing input'});
         throw err;
     }
-    
 });
 
 app.listen(8675, () => console.log('Server ready and listening'));
